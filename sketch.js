87e5a7ba572389b20c9cc9289406228af6602811
nwilliams8 <33866591+nwilliams8@users.noreply.github.com>
2017-11-27 15:12:06 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,63 +1,29 @@
-//https://p5js.org
-//https://p5js.org/learn/
 
-//DAT405 / GAD405
-//03_Colorlines (Exercise 01)
-//Link all circles with individual lines
-//Color of the line is the mix of the circle colors
-
-//Initialization function
+let size = 50;
 function setup() {
-  //Set the size of rendering window - pixels
-  createCanvas(700, 400);
-
-  //Size of stroke
-  strokeWeight(7);
-  background(100);
+  createCanvas(505, 505);
   //No stroke for shapes
 }
 
 //Rendering function
 function draw() {
-  //Erase all canvas. Set the color to white
-  // top left to top right
-  stroke(255,0,255, 5);
-  line(50,50, width-50, 50);
-  // top left tp bottom left
-  stroke(0,255,255, 5);
-  line(50,50, 50, height-50);
-  // top right to bottom right
-  stroke(255,165,0, 5);
-  line(width-50, 50, width-50, height-50);
-  // bottom left to bottom right
-  stroke(255,255,0, 5);
-  line(50, height-50, width-50, height-50);
-  // top left to bottom right
-  stroke(0,255,0 , 5);
-  line(50, 50, width-50, height-50);
+  background(0);
+  var mouse1 = map(mouseY, 0, height, 0, 255);
+  var mouse2 = map(mouseX, 0, width, 0, 255);
+  var mouse3 = map(mouseY, 0, width, 255, 0);
 
+  var color1 = color(255, 37, 54, 255);
+  var color2 = color(26, 194, 203, mouse1);
+  var color3 = color(26, 194, 203, mouse2);
+  var color4 = color(239, 108, 46, mouse3);
 
-
-
-  noStroke();
-
-  //top left
-  fill(0, 0, 255);
-  ellipse(50, 50, 100, 100);
-
-
-  //Top right corner shape position and color
-  fill(255, 0, 0);
-  ellipse(width-50, 50, 100, 100);
-
-
-  //Bottom left corner shape position and color
-  fill(0, 255, 0);
-  ellipse(50, height-50, 100, 100);
-
-
-  //Bottom right corner shape position and color
-  fill(255, 255, 0);
-  ellipse(width-50, height-50, 100, 100);
-
+    for (let y = 0; y < height; y+=size) {
+      for (let x = 0; x < width; x+=size){
+        if (mouseX>x && mouseX<x+size && mouseY>y && mouseY<y+size) fill(color1);
+        else if (mouseX>x && mouseX<x+size) fill(color2);
+        else if (mouseY>y && mouseY<y+size) fill(color3);
+        else fill(color4);
+        rect(x, y, size, size);
+      }
+    }
 }
