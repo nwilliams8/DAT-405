4449d4a6e7fb98d9e02adcfbb9cff966ad752123
nwilliams8 <33866591+nwilliams8@users.noreply.github.com>
2017-12-06 19:41:43 +0000

    change2

--- Archive/sketch.js
+++ Archive/sketch.js
@@ -5,6 +5,8 @@ let arraySize = 1000;
 function setup() {
   createCanvas(500, 500);
   background(160);
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(500, 500)
   for (let i=0; i<arraySize; i++){
     circleArray[i] = new Circle(width/2, height/2, random(-0.05, 0.05), random(-0.05, 0.05), random(1, 10));
   }
@@ -22,22 +24,18 @@ class Circle{
 
   constructor(x, y, speedX, speedY, size){
     this.angle = 0
-    //Setup of class' variables
+    //Setup of class' variabl
     this.x = x;
     this.y = y;
     this.speedX = speedX;
     this.speedY = speedY;
     this.size = size;
     this.a = 255;
-    this.rd = random(255);
+    this.r = random(255);
     this.grn = random(255);
     this.bl = random(255);
-    this.r = random(255);
     this.g = random(255);
     this.b = random(255);
-    this.from = color(this.rd, this.grn, this.bl);
-    this.to = color(this.r, this.g, this.b);
-    this.fillcol = lerpColor(this.from, this.to, random(1))
   }
 
   //Class function that takes care of motion and collision
@@ -64,6 +62,8 @@ class Circle{
   //Class function that displays the ellipse
   displayCircle(){
     noStroke()
+    this.a = rSlider.value()
+    this.fillcol = color(this.r, this.g, this.b, this.a)
     fill(this.fillcol);
     ellipse(this.x, this.y, this.size, this.size);
   }
